3056be97587c7fb487bd24793d027cd64226f31b
joshuadmello04 <joshuadmello777@gmail.com>
2026-02-19 15:02:06 +0530

    Added tax calculation in master branch

--- git-conflict-demo/App.java
+++ git-conflict-demo/App.java
@@ -1,6 +1,11 @@
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Application Started");
+        int price = 1000;
+        System.out.println("Final Price: " + applyTax(price));
+    }
+
+    public static int applyTax(int price) {
+        return price + 180;
     }
 }
